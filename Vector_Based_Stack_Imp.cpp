e1232eaf1a0dbe0e6e03c1313ec69e8c54fa6a09
jsb58p <jacobbiddinger@gmail.com>
2024-09-30 08:05:12 -0500

    Update Vector_Based_Stack_Imp.cpp

--- Vector_Based_Stack_Imp.cpp
+++ Vector_Based_Stack_Imp.cpp
@@ -1,1 +1,14 @@
+#include "Vector_Based_Stack.h"
 
+int average(std::stack<int, std::vector<int>> theStack) { //finds the average value of the elements
+	/*Find Average Value of stack elements*/
+	int size = theStack.size(); //set av to the size of the list, will be divided from sum at the end for the real average
+	int sum = 0;		//sum of the numbers, starts with 0
+
+	while (!theStack.empty()) {		//while list is not empty, add the top number, pop it, add the new top number, etc.
+		sum = sum + theStack.top();
+		theStack.pop();
+	}
+	size = sum / size;			//average is the sum divided by the size of the list
+	return size;
+}
