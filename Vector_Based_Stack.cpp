46874a945587ec19d3d7b82de0c79c04aa060c0b
jsb58p <jacobbiddinger@gmail.com>
2024-09-30 08:05:27 -0500

    Update Vector_Based_Stack.cpp

--- Vector_Based_Stack.cpp
+++ Vector_Based_Stack.cpp
@@ -1,8 +1,5 @@
-#include <stack>
-#include <vector>
 #include <iostream>
-
-int average(std::stack<int, std::vector<int>>); //function header for finding average value of stack elements
+#include "Vector_Based_Stack.h"
 
 int main() {
 	std::stack<int, std::vector<int>> numbers; //create a stack object using a vector container for holding integers
@@ -23,24 +20,8 @@ int main() {
 	/*Find the top of the stack*/
 	std::cout << numbers.top() << std::endl;
 
-	/*Find the average value of the stack elements*/
-	std::cout<< average(numbers)<<std::endl;
-
-	/*Make sure the stack is still the same size*/
+	std::cout << average(numbers) << std::endl;
 	std::cout << numbers.size();
-	
-	
-}
 
-int average(std::stack<int, std::vector<int>> theStack) { //finds the average value of the stack elements
-	/*Find Average Value of stack elements*/
-	int size = theStack.size(); //set av to the size of the list, will be divided from sum at the end for the real average
-	int sum = 0;		//sum of the numbers, starts with 0
 
-	while (!theStack.empty()) {		//while list is not empty, add the top number, pop it, add the new top number, etc.
-		sum = sum + theStack.top();
-		theStack.pop();
-	}
-	size = sum / size;			//average is the sum divided by the size of the list
-	return size;
 }
