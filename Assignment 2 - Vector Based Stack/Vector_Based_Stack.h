1b965ffb8c18976b213e0cd8e2208ea3c31fbeef
jsb58p <jacobbiddinger@gmail.com>
2024-10-01 09:18:56 -0500

    Create Vector_Based_Stack.h

---
+++ Assignment 2 - Vector Based Stack/Vector_Based_Stack.h
@@ -0,0 +1,26 @@
+#ifndef VECTOR_BASED_STACK_H_
+#define VECTOR_BASED_STACK_H_
+#include <vector>
+#include <cstddef>
+
+template<typename Item_Type>
+class vector_based_stack {
+public:
+	void push(const Item_Type&);
+	Item_Type& top();
+	const Item_Type& top() const;
+	void pop();
+	bool empty() const;
+	size_t size() const;
+	int average(); //function header for finding average
+
+	vector_based_stack() {}
+private:
+	std::vector<Item_Type> container;
+
+};
+
+
+#include "Vector_Based_Stack_Imp.tpp"
+
+#endif
