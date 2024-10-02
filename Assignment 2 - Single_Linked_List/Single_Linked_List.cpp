6482dd2f05872a0e964355d99c46d18a3742e5b6
jsb58p <jacobbiddinger@gmail.com>
2024-10-01 22:24:54 -0500

    Update Single_Linked_List.cpp

--- Assignment 2 - Single_Linked_List/Single_Linked_List.cpp
+++ Assignment 2 - Single_Linked_List/Single_Linked_List.cpp
@@ -4,32 +4,32 @@
 
 int main() {
 
-    Single_Linked_List<std::string> list;
+    Single_Linked_List<std::string> list;         //create a Single Linked List object
 
-    list.push_front("ABC");
-    list.push_back("DEF");
-    list.insert(1, "GHI");
+    list.push_front("ABC");            //push item to front of list.
+    list.push_back("DEF");            //push item to back of list.
+    list.insert(1, "GHI");            //insert item at position 1, before the item that was previously at position 1
 
-    std::cout << list.front() << std::endl;
-    std::cout << list.back() << std::endl;
+    std::cout << list.front() << std::endl;        //output the front of the list (position 0)
+    std::cout << list.back() << std::endl;         //output the back of the list.
 
-    list.pop_back();
+    list.pop_back();            //remove the back of the list.
 
-    std::cout << list.back() << std::endl;
+    std::cout << list.back() << std::endl;        //output the new back of the list.
 
-    list.pop_front();
+    list.pop_front();                //remove the front of the list.
 
-    std::cout << list.front() << std::endl;
+    std::cout << list.front() << std::endl;        //output the new front of the list. Since there is only one item left, the front and the back of the list are now the same.
 
-    std::cout << list.empty() << std::endl;
+    std::cout << list.empty() << std::endl;        //check if the list is empty (it isn't, there is one item left)
 
-    std::cout << list.remove(1) << std::endl;
+    std::cout << list.remove(1) << std::endl;       //attempts to remove an item at position 1. The output indicates whether the removal is successful. There is only one item at position 0 so it is not successful.  
 
-    std::cout << list.find("GHI") << std::endl;
+    std::cout << list.find("GHI") << std::endl;    //finds the position of the item "GHI"
 
-    std::cout << list.remove(0) << std::endl;
+    std::cout << list.remove(0) << std::endl;        //successfully removes the remaining item at position 0. 
 
-    std::cout << list.empty() << std::endl;
+    std::cout << list.empty() << std::endl;        //checks if list is empty. It is. 
 
 
     return 0;
